7cec9c082ffa8b8d3a158893aabe38af7c8dd647
MohamedBoutaieb <68185589+MohamedBoutaieb@users.noreply.github.com>
2021-04-10 12:06:10 +0100

    Add files via upload

---
+++ accueil/form-script.js
@@ -0,0 +1,40 @@
+
+var pass = document.querySelector("#password");
+var confirme = document.querySelector("#confirmPass");
+function invalid(event){
+    if(  event.target.value.length <8  && pass.className!="form-control is-invalid") {
+        pass.className="form-control is-invalid";
+        document.querySelector("#warning").textContent="password must contain at least 8 characters";
+    
+     
+  }
+  
+  else if (  event.target.value.length >=8  && pass.className=="form-control is-invalid") {
+        pass.className="form-control";
+        document.querySelector("invalid-feedback").textContent="" ; }
+}
+function match (event){
+  if (pass.value!= event.target.value && event.target.className!="form-control is-invalid")
+  {
+    event.target.className="form-control is-invalid";
+    document.querySelector("#conf").textContent="passwords do not match";
+  }
+  else if (pass.value== event.target.value && event.target.className!="form-control is-valid")
+  {
+    event.target.className="form-control is-valid";
+  }
+
+
+}
+pass.addEventListener("input",invalid);
+confirme.addEventListener("input",match);
+var lastScrollTop=0;
+var nav = document.querySelector("header");
+window.addEventListener("scroll",function(){
+  var scrollTop=window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop> lastScrollTop){
+    nav.style.top="-300px";
+  }
+  else{ nav.style.top="0px";}
+  lastScrollTop=scrollTop;
+})
