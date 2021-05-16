2ad9dd0acaa1fcdc9d5c16173cb73536589853e3
Aziz <mohamedazizkhayati@insat.u-carthage.tn>
2021-05-17 00:06:13 +0100

    changes

--- src/Controller/Admin/PostsController.php
+++ src/Controller/Admin/PostsController.php
@@ -4,9 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\Article;
 use App\Entity\User;
+use App\Form\EditPostType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -35,24 +37,23 @@ class PostsController extends AbstractController
         $post->setActive(($post->getActive())?false:true);
         $manager->persist($post);
         $manager->flush();
-       return new Response("true");
+       return $this->redirectToRoute("admin_posts_home");
     }
+
     /**
      * @Route("/Edit/{post}",name="_edit_post")
      */
 
-    public function Edit(Article $post = null, EntityManagerInterface $manager)
+    public function edit(Article $post, EntityManagerInterface $manager, Request $request)
     {
-
-        if ($post) {
-            $manager->remove($post);
+        $form = $this->createForm(EditPostType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isvalid()) {
             $manager->flush();
-            $this->addFlash('success', "The post has been successfully deleted");
-        } else {
-            $this->addFlash('error', "The post does not exist");
+            $this->addFlash('success', 'This article has been successfully updated');
+            return $this->redirectToRoute('admin_posts_home');
         }
-
-        return $this->redirectToRoute('admin_posts_home');
+        return $this->render('account_details/editArticle.html.twig', ['form' => $form->createView()]);
     }
 
     /**
