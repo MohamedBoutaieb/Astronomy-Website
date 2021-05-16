132d0205c85368615bef3bec020e68af49279731
Aziz <mohamedazizkhayati@insat.u-carthage.tn>
2021-05-16 19:30:55 +0100

    changes

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfileController extends AbstractController
+{
+    /**
+     * @Route("/search/page={page<\d+>?1}" ,name="search")
+     */
+    public function search (EntityManagerInterface $manager, Request $request, $page): Response
+    {
+        $a="h";
+        $userRepo = $manager->getRepository('App:User');
+        if($request->isMethod("get")){
+            $users = $userRepo->search($_GET["user"]);
+            dd($users);
+            return $this->redirectToRoute('homepage');
+        }
+        else{
+            return $this->redirectToRoute('homepage');
+        }
+    }
+}
