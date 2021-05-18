d6814bb0e40dcede69367c1f5a8714672bfdc977
Aziz <mohamedazizkhayati@insat.u-carthage.tn>
2021-05-18 20:55:25 +0100

    changes

---
+++ src/Services/UserSessionUpdater.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class UserSessionUpdater
+{
+    private $session, $registry;
+    public function __construct(SessionInterface $session, ManagerRegistry $registry){
+        $this->session = $session;
+        $this->registry = $registry;
+    }
+    public function updateUserSession(){
+        if($this->session->has('username')){
+            $repository = $this->registry->getRepository(User::class);
+            $user = $repository->findOneByUsername($this->session->get('username'));
+            $this->session->set('user', $user);
+            return $user;
+        }
+    }
+}
