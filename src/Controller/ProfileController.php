86dc3f2b67930dcf9dd259722663f00a8ff02b97
Mohamed Boutaïeb <mohamedboutaieb1459@gmail.com>
2021-05-19 15:12:36 +0100

    search page

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
@@ -13,17 +14,38 @@ class ProfileController extends AbstractController
     /**
      * @Route("/search/page={page<\d+>?1}" ,name="search")
      */
-    public function search (EntityManagerInterface $manager, Request $request, $page): Response
+    public function search (EntityManagerInterface $manager,SessionInterface $session ,Request $request, $page): Response
     {
-        $a="h";
+
         $userRepo = $manager->getRepository('App:User');
         if($request->isMethod("get")){
-            $users = $userRepo->search($_GET["user"]);
+            if (isset($_GET["user"]))
+            {$session->set('keysearch',$_GET["user"]);}
+            $users = $userRepo->search($session->get('keysearch'));
 
-            return $this->redirectToRoute('searchpage');
+
+
+          if ($users!=null)
+            return $this->render('profile/search_result.html.twig', [
+               'users'=> $users
+            ]);
+            return $this->redirectToRoute('homepage');
         }
         else{
-            return $this->redirectToRoute('searchpage');
+            return $this->redirectToRoute('homepage');
         }
     }
+    /**
+     * @Route("/details/{id}" ,name="details")
+     */
+    public function profileDetails(EntityManagerInterface $manager,$id, Request $request,SessionInterface $session): Response
+    { $userRepo = $manager->getRepository('App:User');
+        $user = $userRepo->findOneBy(['username'=>$id]);
+
+        return $this->render('profile/index.html.twig', [
+            'user'=> $user
+        ]);
+    }
+
+
 }
